637443beb16b08428ba04272fb75adde05f6bcac
Bekmurod Urinbekov <116829142+bekmurod2728@users.noreply.github.com>
2024-01-26 09:41:55 +0500

    Update Main3.java

--- src/main/java/bot/download/Main3.java
+++ src/main/java/bot/download/Main3.java
@@ -7,26 +7,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Main3 {
-    public static void main(String[] args) {
-        int[] ratings={1,2,2};
-
-            int sum=0;
-            for(int i=0;i<ratings.length;i++){
-                int j=i;
-                if((j+1)!= ratings.length && ratings[j]>ratings[j+1]){
-                    sum+=2;
-                }else if((j-1)>=0 && ratings[i-1]<ratings[i]){
-                    sum+=2;
-                }else sum++;
-            }
-        System.out.println(sum);
-        System.out.println("commite name saved");
-        System.out.println("conflickt");
-        System.out.println(100000);
-        System.out.println(66667);
-        System.out.println("gggggggg");
-        System.out.println("sadfsdf");
-        System.out.println(999999);
-    }
+    
 }
 
